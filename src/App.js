e198f861e5bb81d3a2845d6f690d2123135664a1
dchima <dcnnad@gmail.com>
2021-03-13 12:26:30 +0100

    finish

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
+import { isAndroid, isIOS, isFirefox } from 'react-device-detect';
 import logo from './logo.svg';
+import logo1 from './logo.png';
 import './App.css';
 
 function App() {
+  // if (isFirefox) window.location.replace('https://www.google.com');
+  if (isIOS) window.location.replace('https://apps.apple.com/ng/app/keymobile/id1332114961')
+  if (isAndroid) window.location.replace('https://play.google.com/store/apps/details?id=com.qucoon.keystonemobilebankingapp&hl=en&gl=US')
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo1} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          redirecting you to our mobile app...
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
